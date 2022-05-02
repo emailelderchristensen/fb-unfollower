98801c5debd3e4b6a8eadc92dfe4f54e24a07387
emailelderchristensen <69782973+emailelderchristensen@users.noreply.github.com>
2022-05-02 07:38:19 -0400

    save v0.1

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 
 // let friendsContainer = $("[aira-label='All friends']");
 // let betterFriendsContainer = $(".sxpk6l6v");
-let evenBetterSelector = $("div[aria-label='More']");
+let moreButtons = $("div[aria-label='More']");
 let popoverDiv = $("div")
-// evenBetterSelector.forEach((child) => {
-//     child.click();
-// })
+let regex = RegExp(/Unfollow (\w+)/g)
+let randDelay = function() {
+  return Math.floor(Math.random() * 100)
+}
 
 function clickMore(el) {
     el.click();
     console.log("clicked");
 
-    popoverDiv.find("span")
+    let unfollowed = false;
+    popoverDiv.find("span").each((i, menuItem) => {
+      if (!unfollowed && regex.test(menuItem.innerText)) {
+        unfollowed = true;
+        setTimeout(() => {
+          clickUnfollow(menuItem);
+        }, 400 + randDelay());
+      }
+    })
+    if (!unfollowed) {
+      //Click whatever div closes modals
+    }
 }
 
 function clickUnfollow(el) {
-    
+  el.click();
+  console.log("clicked: " + el.innerText);
 }
 
 const delayLoop = (fn, delay) => {
     return (i, element) => {
       setTimeout(() => {
         fn(element);
-      }, i * delay);
+      }, i * (delay + randDelay()));
     }
   };
   
-  evenBetterSelector.each(delayLoop(clickMore, 1000));
+  moreButtons.each(delayLoop(clickMore, 1000));
