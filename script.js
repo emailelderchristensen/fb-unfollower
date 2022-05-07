20bb097ec490286aa9e1b55037e3feb3d8a3afe7
emailelderchristensen <69782973+emailelderchristensen@users.noreply.github.com>
2022-05-07 17:09:49 -0400

    save bc thunder

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Constants
-let moreButtons = $("div[aria-label='More']"); //selects all the ... buttons
-let popoverDiv = $("body > div > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div:nth-child(2)") // selects the div 
-let regex = RegExp(/Unfollow (\w+)/g) // Matches "Unfollow *****"
+const popoverDiv = $("body > div > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div:nth-child(2)") // selects the div 
+const regex = RegExp(/Unfollow (\w+)/g) // Matches "Unfollow *****"
 
 // Helper Functions
 let randDelay = () => Math.floor(Math.random() * 100) // Helper function that generates random 100ms delays
@@ -15,38 +14,49 @@ let delayLoop = (fn, delay) => {
   }
 };
 
+async function unfollower() {
+  let moreButtons = $("div[aria-label='More']");
+  let updateButtons = () => {moreButtons = $("div[aria-label='More']")} //selects all the ... buttons
+  for (i = 0; i < moreButtons.length; i++) {
+    
+  }
+}
+
 async function clickMore(el) {
   let unfollowed = false;
   let foundButtons = [];
 
-  console.log("Calling ClickMore")
+  // console.log("Calling ClickMore")
+  el.scrollIntoView({behavior: "smooth", block: "center"})
   el.click();
-  console.log("clicked three dots")
+  // console.log("clicked three dots")
 
-  await sleep(500) // Wait until DOM updates
+  await sleep(1000) // Wait until DOM updates
 
-  console.log("sleep Finished: ", popoverDiv.find("span"));
+  // console.log("sleep Finished: ", popoverDiv.find("span"));
 
   popoverDiv.find("span").each((i, menuItem) => {
-    console.log("Testing: " + menuItem.innerText)
-    console.log(regex.test(menuItem.innerText))
-    if (!unfollowed && regex.test(menuItem.innerText)) {
-      foundButtons.append(menuItem)
+    // console.log("Testing: " + menuItem.innerText, " -> ", regex.test(menuItem.innerText))
+    // console.log(regex.test(menuItem.innerText))
+    if (regex.test(menuItem.innerText)) {
+      console.log("Found ", menuItem.innerText, " unfollowing...")
+      foundButtons.push(menuItem)
       unfollowed = true;
+      // console.log("Found Buttons: ", foundButtons);
+      // console.log("unfollowed: ", unfollowed);
     }
   })
   if (!unfollowed) {
     console.log("no unfollow text found");
+    el.click(); //close Modals
   }
   
-  console.log("Setting Timeout");
+  // console.log("Setting Timeout");
   let toDo = () => {
     console.log("Clicking these Buttons: ", foundButtons)
     clickUnfollow(foundButtons);
-    await sleep(200)
-    el.click() //closeModals
   }
-  setTimeout(toDo, 400 + randDelay());
+  setTimeout(toDo, 1000 + randDelay());
 }
 
 function clickUnfollow(elements) {
@@ -57,4 +67,4 @@ function clickUnfollow(elements) {
 }
 
 // Run  
-moreButtons.each(delayLoop(clickMore, 10000));
+unfollower();
