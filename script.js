d88b17c8e61f9e081e9c59e8ba2adf7ecb898647
emailelderchristensen <69782973+emailelderchristensen@users.noreply.github.com>
2022-05-13 11:53:26 -0400

    working??

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const popoverDiv = $("body > div > div > div:nth-child(1) > div > div.rq0escxv.l
 const regex = RegExp(/Unfollow (\w+)/g) // Matches "Unfollow *****"
 const friends = $("body > div > div > div:nth-child(1) > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb.ka73uehy > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7 > div > div.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.du4w35lb.q5bimw55.ofs802cu.pohlnb88.dkue75c7.mb9wzai9.d8ncny3e.buofh1pr.g5gj957u.tgvbjcpo.l56l04vs.r57mb794.kh7kg01d.eg9m0zos.c3g1iek1.l9j0dhe7.k4xni2cv > div.j83agx80.cbu4d94t.buofh1pr.l9j0dhe7 > div.aov4n071 > div > div.rtmu0sqt.qzhwtbm6 > div > div > div > div > div > h2 > span > span")
 const friendsRegex = RegExp(/([\d]+,[\d]+) friends/g) // Matches "123 friends"
+
+//Globals
+let moreButtons = $("div[aria-label='More']");
+let updateButtons = () => {moreButtons = $("div[aria-label='More']")}
+let go = false;
+
 // Helper Functions
 let randDelay = () => Math.floor(Math.random() * 100) // Helper function that generates random 100ms delays
 let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)); // Helper function that sleeps for a ms
@@ -18,12 +24,33 @@ let delayLoop = (fn, delay) => {
   }
 };
 
+async function waitForFriends(numberOfFriends) {
+  let i = 0
+  let previousButtonCount = 0;
+  do {
+    if (go === true) { return true}
+    let previousButtonCount = moreButtons.length;
+    updateButtons();
+    if (previousButtonCount <= moreButtons.length) {
+      i++;
+    }
+    previousButtonCount = moreButtons.length
+    
+    console.log("Keep Scrolling up and down the friends list! Found " + moreButtons.length+"/"+ numberOfFriends + " Friends so far...")
+    if (i > 5) {
+      console.log("Seems like you're not getting any new friends... Type \"start = true\" and press enter to continue anyway.")
+      i = 0
+    }
+    await sleep(5000);
+  } while (numberOfFriends - 10 > moreButtons.length);
+}
+
 async function unfollower() {
-  let moreButtons = $("div[aria-label='More']");
-  let updateButtons = () => {moreButtons = $("div[aria-label='More']")} //selects all the ... buttons
+ //selects all the ... buttons
   let numberOfFriends = parseInt(friendsRegex.exec(friends.text())[1].replace(',',''))
   let i = 0;
-  console.log(numberOfFriends);
+  console.log("I found that you have " + numberOfFriends + " Friends");
+  await waitForFriends(numberOfFriends);
   while(i < numberOfFriends) {
     console.log("Looping!")
     if (moreButtons.length > i) {
@@ -37,13 +64,14 @@ async function unfollower() {
     } else {
       moreButtons[i - 20]
       .scrollIntoView({behavior: "smooth", block: "center"});
-      await sleep(1000 + randDelay());
+      await sleep(2000 + randDelay());
       moreButtons[i - 1]
       .scrollIntoView({behavior: "smooth", block: "center"}) //scroll the user to the center
+      await sleep(2000 + randDelay());
     }
     await sleep(500 + randDelay());
-    updateButtons();
-    console.log("Looped: ", i, " times");
+    // updateButtons();
+    // console.log("Looped: ", i, " times");
   }
   return "Done!"
 }
